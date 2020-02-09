13e51b2445c65eefbe8409df8cc96f06c74c5b1e
ravczar <ravczar@gmail.com>
2020-02-09 14:53:46 +0100

    add folders

---
+++ MechanicSolution/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MechanicSolution.Models
+{
+    public abstract class Item
+    {
+        private static uint Iterator;
+        public uint Id { get; set; }
+        public DateTime DateOfProduction { get; set; }
+
+        public Item() => this.Id = Iterator++;
+
+        public abstract void ExpressYourself();
+
+    }
+}
