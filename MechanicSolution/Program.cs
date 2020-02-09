56a7570326d6459097ba4ca55abdc9976757f2b2
ravczar <ravczar@gmail.com>
2020-02-09 15:27:03 +0100

    adding some initial program to see it works fine

--- MechanicSolution/Program.cs
+++ MechanicSolution/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MechanicSolution.Interfaces;
+using MechanicSolution.Models;
+using System;
+using System.Collections;
+
 
 namespace MechanicSolution
 {
@@ -6,7 +10,22 @@ namespace MechanicSolution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car car = new Car(DateTime.Now, "Panda", "Fiat");
+            Bike bike = new Bike(DateTime.Now.AddDays(3), "GS600", "Honda");
+
+            ArrayList ItemArray = new ArrayList();
+            ItemArray.Add(car);
+            ItemArray.Add(bike);
+
+            foreach(IItem item in ItemArray)
+            {
+                Console.WriteLine(item);
+            }
+
+            
+
+            
+
         }
     }
 }
